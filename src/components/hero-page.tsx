a6651d5a151bacc802b91c2e486b5ed1860d66c6
devbertskie <bert.stark3231@gmail.com>
2024-05-24 16:00:53 +0800

    feat: add text-animation

--- src/components/hero-page.tsx
+++ src/components/hero-page.tsx
@@ -17,13 +17,12 @@ export default function HeroPage() {
         priority={true}
       />
       <div className="flex flex-col items-center space-y-4">
-        <h1 className="text-3xl md:text-5xl">
+        <h1 className="typing-demo text-3xl md:text-5xl">
           Hi, I&apos;m{" "}
           <span className="font-bold capitalize text-primary">Hubert</span>👋{" "}
         </h1>
         <p className="max-w-xl text-center text-sm text-muted-foreground sm:text-lg">
-          Passionate developer crafting intuitive digital solutions with
-          dedication.
+          {siteConfig.careerOnjective}
         </p>
       </div>
       <div className="flex flex-row space-x-4">
